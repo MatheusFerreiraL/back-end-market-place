90adb7b886ecbdfe254a54d9f9d0ae1134d4b164
MatheusFerreiraL <matheusfleandro@gmail.com>
2023-01-30 15:58:04 -0300

    :sparkles: feat: rota login e autenticacao

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const loginStore = require('./controllers/loginStore');
 const { registerStore } = require('./controllers/registerStore');
+const loggedAuth = require('./middlewares/loggedAuth');
 const verifyRequestBody = require('./middlewares/verifyRequestBody');
+const storeLoginSchema = require('./schemas/storeLoginSchema');
 const storeSchema = require('./schemas/storeSchema');
 
 const routes = express();
 
 routes.post('/register', verifyRequestBody(storeSchema), registerStore);
+routes.post('/login', verifyRequestBody(storeLoginSchema), loginStore);
+
+routes.use(loggedAuth);
+
+routes.get('/', (req, res) => {
+  return res.json(req.store);
+});
 
 module.exports = routes;
